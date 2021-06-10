aa9fdfe026c70f81ba1f6263903fb0a77e36b17c
JoaoErick <jerick1415@outlook.com>
2021-06-10 00:19:27 -0300

    First day.

---
+++ src/pages/index.tsx
@@ -0,0 +1,35 @@
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+
+export default function Home() {
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+      <UserInformation
+        name={"João Erick Barbosa"}
+        picture={"https://github.com/JoaoErick.png"}
+        rating={1}
+        description={"Serrinha"}
+      />
+      <UserInformation
+        name={"João Erick Barbosa"}
+        picture={"https://github.com/JoaoErick.png"}
+        rating={3}
+        description={"Serrinha"}
+      />
+      <UserInformation
+        name={"João Erick Barbosa"}
+        picture={"https://github.com/JoaoErick.png"}
+        rating={4}
+        description={"Serrinha"}
+      />
+    </div>
+  );
+}
