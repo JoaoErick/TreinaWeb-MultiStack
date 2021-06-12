5430aa4fdeae96a0a0c965da08c625cfce4b30b1
JoaoErick <jerick1415@outlook.com>
2021-06-12 18:19:50 -0300

    Second day.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextField from 'ui/components/inputs/TextField/TextField';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
+import { Button, Typography } from '@material-ui/core';
+import { FormElementsContainer, ProfissionaisPaper,  ProfissionaisContainer} from '@styles/pages/index.style';
 
 export default function Home() {
   return (
@@ -12,24 +16,64 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <UserInformation
-        name={"João Erick Barbosa"}
-        picture={"https://github.com/JoaoErick.png"}
-        rating={1}
-        description={"Serrinha"}
-      />
-      <UserInformation
-        name={"João Erick Barbosa"}
-        picture={"https://github.com/JoaoErick.png"}
-        rating={3}
-        description={"Serrinha"}
-      />
-      <UserInformation
-        name={"João Erick Barbosa"}
-        picture={"https://github.com/JoaoErick.png"}
-        rating={4}
-        description={"Serrinha"}
-      />
+      <FormElementsContainer>
+        <TextFieldMask 
+          mask={'99.999-999'} 
+          label={'Digite seu CEP'} 
+          fullWidth 
+          variant={'outlined'}
+        />
+        <Typography color={'error'}>CEP inválido</Typography>
+        <Button 
+        variant={'contained'} 
+        color={'secondary'} 
+        sx={{width: '220px'}}
+        >
+          Buscar
+        </Button>
+      </FormElementsContainer>
+
+      <ProfissionaisPaper>
+        <ProfissionaisContainer>
+          <UserInformation
+            name={"João Erick Barbosa"}
+            picture={"https://github.com/JoaoErick.png"}
+            rating={1}
+            description={"Serrinha"}
+          />
+          <UserInformation
+            name={"João Erick Barbosa"}
+            picture={"https://github.com/JoaoErick.png"}
+            rating={3}
+            description={"Serrinha"}
+          />
+          <UserInformation
+            name={"João Erick Barbosa"}
+            picture={"https://github.com/JoaoErick.png"}
+            rating={4}
+            description={"Serrinha"}
+          />
+          <UserInformation
+            name={"João Erick Barbosa"}
+            picture={"https://github.com/JoaoErick.png"}
+            rating={1}
+            description={"Serrinha"}
+          />
+          <UserInformation
+            name={"João Erick Barbosa"}
+            picture={"https://github.com/JoaoErick.png"}
+            rating={3}
+            description={"Serrinha"}
+          />
+          <UserInformation
+            name={"João Erick Barbosa"}
+            picture={"https://github.com/JoaoErick.png"}
+            rating={4}
+            description={"Serrinha"}
+          />
+        </ProfissionaisContainer>
+      </ProfissionaisPaper>
+      
     </div>
   );
 }
